19cf290e68f7d65e34bad1abb7b95c388a693d4f
Boniface Kimani Muguro <mmbonifacekimani@gmail.com>
2025-03-24 19:57:12 +0300

    completed work

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+// Removing the existing <main> element with id 'main'
+const main = document.getElementById("main");
+if (main) {
+    main.remove();
+}
+
+// new <h1> element
+const newHeader = document.createElement("h1");
+
+// id to 'victory'
+newHeader.id = "victory";
+
+// required message
+newHeader.textContent = "James Damien is the champion";
+
+// Appended new <h1> elementy
+document.body.appendChild(newHeader);
